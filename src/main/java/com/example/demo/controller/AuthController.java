377923fbf92f7e58e9411af14cd496eb97b05e7f
Nguyen Khac Son <creatine09072004@gmail.com>
2026-05-01 11:59:38 +0700

    refactor:security

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
-@RequiredArgsConstructor
+@RequiredArgsConstructor    
 public class AuthController {
 
     private final IAuthService authService;
